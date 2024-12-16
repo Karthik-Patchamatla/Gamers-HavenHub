22a4eb779017e90d5f8a9a4a658ba108c6fbe22f
Karthik-Patchamatla <pkarthikvarma830@gmail.com>
2024-12-17 00:07:42 +0530

    Chnages in the Items Template.

--- Frontend/src/components/ItemsTemplate.jsx
+++ Frontend/src/components/ItemsTemplate.jsx
@@ -9,20 +9,19 @@ function ItemsTemplate({ title, data }) {
                 <h2 className="text-2xl font-semibold">{title}</h2>
                 <img src={underlineImage} alt="Underline" className="w-16 md:w-30 h-auto mt-2" />
             </div>
-            <div className="scrollbar-none overflow-x-auto flex space-x-6 py-6 scrollbar-thin scrollbar-thumb-gray-300 scroll-smooth">
+            <div className="scrollbar-none overflow-x-auto flex space-x-3 py-6 scrollbar-thin scrollbar-thumb-gray-300 scroll-smooth">
                 {data.map((item, index) => (
                     <div 
                         key={index} 
-                        className="flex flex-col items-center rounded-lg bg-[#1e1e1e] py-5 min-h-[150px] min-w-[170px] text-left"
+                        className="flex flex-col items-center rounded-lg bg-[#1e1e1e] px-1 pt-1 min-h-[150px] min-w-[170px] text-left"
                     >
                         <img 
                             src={item.src}
                             alt={item.desc} 
-                            className="w-[170px] h-[150px] object-fill mb-4 shadow-lg"
+                            className="w-[170px] h-[150px] rounded-lg object-fill mb-4 shadow-lg"
                         />
-                        <div className="mt-2 w-full pl-3 pr-3">
-                            <p className="text-[#878789] text-sm text-left">Base Game</p>
-                            <p className="font-semibold text-xs line-clamp-1 mt-2">{item.desc}</p>
+                        <div className="mb-3 w-full px-3">
+                            <p className="font-semibold text-[13px] line-clamp-1 mt-2">{item.desc}</p>
                             <p className="font-semibold text-sm mt-4">{item.prize}</p>
                         </div>
                     </div>
