6279823e5c67ce001948bb571dc755a66e88ecea
Karthik-Patchamatla <pkarthikvarma830@gmail.com>
2024-12-18 01:57:16 +0530

    Ready to deploy backend

--- Backend/index.js
+++ Backend/index.js
@@ -1,29 +1,33 @@
+require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+
 const Game = require("./models/Game");
 const app = express();
+
 // Middleware
 app.use(express.json());
 app.use(cors());
-// Connect to MongoDB
+
+// MongoDB Atlas Connection
 mongoose
-  .connect("mongodb://127.0.0.1:27017/GamersHavenHub")
-  .then(() => console.log("Connected to MongoDB"))
-  .catch((err) => console.log(err));
+  .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("Connected to MongoDB Atlas"))
+  .catch((err) => console.error("Database connection error:", err));
 
-app.listen(8080, () => {
-  console.log("Server running on port 8080");
-});
-
-// API Route
+// API Endpoint
 app.get("/api/games", async (req, res) => {
   const { platform } = req.query;
   try {
     const games = await Game.find({ id1: platform });
     res.json(games);
   } catch (error) {
-    console.error(error);
-    res.status(500).send("Error fetching data");
+    console.error("Error fetching data:", error);
+    res.status(500).send("Internal Server Error");
   }
 });
+
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
