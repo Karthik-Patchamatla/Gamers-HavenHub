a77b1892bef8db92d23b37ccb235081d67c4d531
Karthik-Patchamatla <pkarthikvarma830@gmail.com>
2024-12-19 16:32:29 +0530

    Backend Deployement Setup(7)

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,14 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist', // Ensure this matches your Vercel configuration
+    outDir: 'dist',
+    emptyOutDir: true, 
   },
   server: {
-    port: 3000, // Optional: Define a custom port for local dev if needed
+    port: 5173,
+    open: true, 
   },
-  base: '/', // Use './' if assets fail to load in deployment
+  base: './',
 });
